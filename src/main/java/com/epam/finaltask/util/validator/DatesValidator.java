42f805a648cb40f6bf6b37324b8a96aa5c835418
Oleksandr Panchenko <a.panchenko761@gmail.com>
2024-12-20 17:06:10 +0000

    added dates validator

---
+++ src/main/java/com/epam/finaltask/util/validator/DatesValidator.java
@@ -0,0 +1,16 @@
+package com.epam.finaltask.util.validator;
+
+import com.epam.finaltask.dto.VoucherDTO;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class DatesValidator implements ConstraintValidator<ValidDates, VoucherDTO> {
+    @Override
+    public boolean isValid(VoucherDTO voucherDTO, ConstraintValidatorContext constraintValidatorContext) {
+        if (voucherDTO.getArrivalDate() == null || voucherDTO.getEvictionDate() == null) {
+            return true;
+        }
+        return voucherDTO.getArrivalDate().isAfter(voucherDTO.getEvictionDate()) ||
+                voucherDTO.getEvictionDate().isBefore(voucherDTO.getArrivalDate());
+    }
+}
---
+++ src/main/java/com/epam/finaltask/util/validator/ValidDates.java
@@ -0,0 +1,18 @@
+package com.epam.finaltask.util.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = DatesValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDates {
+    String message() default "Arrival date must not be before eviction date";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
