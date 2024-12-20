b579ac0558c5980c9cf92b0edfed25ba5ad3ec99
Oleksandr Panchenko <a.panchenko761@gmail.com>
2024-12-20 17:04:04 +0000

    added interceptor

---
+++ src/main/java/com/epam/finaltask/interceptor/RestLoggingInterceptor.java
@@ -0,0 +1,36 @@
+package com.epam.finaltask.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@Slf4j
+public class RestLoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) {
+        log.info("HTTP Method: {}, Endpoint: {}{}",
+                request.getMethod(),
+                request.getRequestURI(),
+                request.getQueryString() != null ? "?" + request.getQueryString() : "");
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(@NonNull HttpServletRequest request,
+                                @NonNull HttpServletResponse response,
+                                @NonNull Object handler,
+                                Exception e) {
+        log.info("REST Response - URI: {}, Status: {}, Message: {}",
+                request.getRequestURI(),
+                response.getStatus(),
+                (e != null) ? e.getMessage() : "Response OK");
+    }
+}
